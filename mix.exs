615c2739cf24226dc7397c2c7695b23738eaef28
glejeune <gregoire.lejeune@free.fr>
2016-08-15 12:13:35 +0200

    Bump version to 0.1.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Jwerl.Mixfile do
   def project do
     [
       app: :jwerl,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.2",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
