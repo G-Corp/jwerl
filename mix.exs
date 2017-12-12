4cc5868c689e4cdb4fbf06d0d52eed81a47376e9
glejeune <gregoire.lejeune@free.fr>
2017-12-12 23:19:42 +0100

    Prepare 0.2.0 release

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Jwerl.Mixfile do
   def project do
     [
       app: :jwerl,
-      version: "0.1.3",
+      version: "0.2.0",
       elixir: "~> 1.2",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
