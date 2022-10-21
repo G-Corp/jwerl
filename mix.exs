6e778543ef7470e9e8ae32700f5e5a8cc25e00a4
glejeune <greg@lejeun.es>
2022-10-22 00:57:25 +0200

    [feat] Prepare for 1.2.0

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule Jwerl.Mixfile do
   def project do
     [
       app: :jwerl,
-      version: "1.1.0",
+      version: "1.2.0",
       elixir: "~> 1.2",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
